dd495a98413488a8a9a1a0c56e559c17be0cc7d8
whoji <minli1985@gmail.com>
2019-03-10 23:12:14 -0700

    [doc/dev] added readme; added ML core features.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# very-simple-RL
+# a very simple RL demo
+
+`a WIP project` 
+
+### Introduction
+This is a demo of a very simple RL framework; it consists of these modules:
+* agent
+* enviroment (a built-in simple game)
+* ML core engine (pytorch based)
+* main: a master controller for running the system
+
+### Todo
+* everything
+
+###  Contact
+whoji@null.net
--- ml_core.py
+++ ml_core.py
@@ -1,11 +1,21 @@
 import numpy as np
 import pytorch
 
+
 class Network(object):
     """docstring for Trainer"""
-    def __init__(self):
-        super(Trainer, self).__init__()
-        pass
+    def __init__(self, n_feature, n_hidden_l1, n_hidden_l2, n_output):
+        super(Network, self).__init__()
+        
+        self.l1 = torch.nn.Linear(n_feature, n_hidden_l1)
+        self.l2 = torch.nn.Linear(n_hidden_l1, n_hidden_l2)
+        self.out = torch.nn.Linear(n_hidden_l2, n_output)
+       
+    def forward(self, x):
+        a1 = F.relu(self.l1(x))
+        a2 = F.relu(self.l2(x))
+        o = self.out(a2)
+        return x
         
     def train(self):
         pass
@@ -13,6 +23,22 @@ class Network(object):
     def predict(self):
         pass
 
+class Trainer(object):
+    def __init__(self, x, y):
+        self.net = Network(2, 10, 10, 3)
+        self.optimizer = torch.optim.SGD(net.parameters(), lr=0.02)
+        self.loss_func = torch.nn.CrossEntropyLoss()
+        self.x = x
+        self.y = y
+
+    def train(self, epochs = 100):
+        for t in range(epochs):
+            out = self.net(x)
+            loss = self.loss_func(out, y)
+
+            optimizer.zero_grad()   # clear gradients for next train
+            loss.backward()         # backpropagation, compute gradients
+            optimizer.step()        # apply gradients
 
 
 class ExpReplay(object):
